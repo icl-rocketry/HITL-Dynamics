62e8453670dc3730eb350eb6bdcf3635bb75a526
Garv30pr <garv.gogia23@imperial.ac.uk>
2026-02-03 19:24:02 +0000

    add header file

--- include/ISolver.hpp
+++ include/ISolver.hpp
@@ -1,10 +1,9 @@
 #pragma once
-#include "IRocket.hpp"
 #include "State.hpp"
 
 class ISolver {
 public:
     virtual ~ISolver() = default;
 
-    virtual State Step(IRocket& rocket, const State& current, double dt) = 0;
+    virtual State Step(const State& current, double dt) = 0;
 };
