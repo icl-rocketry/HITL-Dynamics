dce936428ca051d7ab22d43d254455782e4809a3
Daniel Dyoniziak <117684164+dan-dyon@users.noreply.github.com>
2026-03-29 00:54:57 +0000

    Remove branch specific logic

--- main.cpp
+++ main.cpp
@@ -1,37 +1,3 @@
-#include "Aerodynamics.hpp"
-#include "State.hpp"
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <stdexcept>
-using namespace std;
-
 int main() {
-    string input = " ";
-    double MachInput = 0.0;
-
-    DragFromFile aero("cd.csv");
-    double MinMach = aero.GetminMach();
-    double MaxMach = aero.GetmaxMach();
-
-    try{
-        cout << "enter a Mach Number(" << MinMach << "<= M <=" << MaxMach << "): ";
-        getline(cin, input);
-        MachInput = stod(input);
-        if (MachInput < 0.00 || MachInput > 3.00) throw logic_error (" Value of Mach number should be in the range");
-    }
-    catch(const exception& e)
-    {
-        cout << "Error in the input" << e.what() << endl;
-        return 1;
-    }
-
-    State state(1);
-    state[0] = MachInput;
-
-    double cd = aero.GetCd(state);
-
-    cout << fixed << setprecision(5);
-    cout << "Drag coefficient: " << cd << endl;
     return 0;
 }
