10aeea44c0d11e875b0179b0b4aa291bc9a87d6a
Yuze Sun <yuze.sun24@imperial.ac.uk>
2026-06-13 11:19:46 +0100

    main.cpp
    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,41 @@
 using namespace std;
 
 int main() {
-    double t = 7.0; //unit: s
-    double total_propmass = 24.4; //unit: kg
+    double total_propmass = 24.4;  // kg
+    double drymass = 60.2;         // kg
+
     IPropulsion engine("Thrust_curve.csv");
-    double T = engine.GetThrust(t);
-    double total_Impulse = engine.calculate_total_impulse();
-    double m_prop = engine.Get_PropMass(total_propmass, t);
+    engine.SetInitialMass(total_propmass);
+
+    double time = 0.0;
+    double total_mass = total_propmass + drymass;
 
     cout << fixed << setprecision(5);
-    cout << "Prop mass: " << m_prop << endl;
-    cout << "Thrust: " << T << endl;
-    cout << "Total impulse: " << total_Impulse << endl;
-    return 0;
+    cout << "Start simulation...\n\n";
+
+    while (time <= 10)
+    {
+        double thrust = engine.GetThrust();
+        double remaining_prop = engine.Get_PropMass(total_propmass, thrust);
+        total_mass = drymass + remaining_prop;
+
+        cout << "t = " << time << " s"
+             << ", Thrust = " << thrust << " N"
+             << ", Prop mass = " << remaining_prop << " kg"
+             << ", Total mass = " << total_mass << " kg"
+             << endl;
+
+        time += 0.01;
+    }
+
+
+    cout << "\n===== FINAL RESULT =====\n";
+    double final_prop = engine.Get_PropMass(total_propmass, 0.0);
+
+    cout << "Final propellant mass: " << final_prop << " kg\n";
+    cout << "Dry mass: " << drymass << " kg\n";
+    cout << "Total impulse: " << engine.calculate_total_impulse() << " N*s\n";
+    cout << "Final vehicle mass: " << drymass + final_prop << " kg\n";
+
     return 0;
 }
